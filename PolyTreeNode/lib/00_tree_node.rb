3007d0ef0114877b4bc198fb7b863817ecf7335d
davidFox <djfoxy11@gmail.com>
2020-09-29 09:01:10 -0500

    Completed phases 0,1,2

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    attr_reader :parent
+    attr_accessor :value
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent)
+        return if self.parent == parent
+        if self.parent
+            self.parent._children.delete(self)
+        end
+
+        @parent = parent
+        self.parent._children << self unless self.parent.nil?
+        self
+    end
 end
