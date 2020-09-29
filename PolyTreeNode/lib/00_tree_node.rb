1b6fb4803d38b268bff5c11c16eac12d4b2857e0
davidFox <djfoxy11@gmail.com>
2020-09-29 09:29:52 -0500

    Created searchable module

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,4 +1,38 @@
+module Searchable
+    def dfs(target = nil, &prc)
+        raise "Need proc or target" if [target, prc].none?
+        prc ||= Proc.new { |node| node.value == target }
+
+        return self if prc.call(self)
+        children.each do |child|
+            result = child.dfs(&prc)
+            return result unless result.nil?
+        end
+        nil
+    end
+
+    def bfs(target = nil, &prc)
+        raise "Need proc or target" if [target, prc].none?
+        prc ||= Proc.new { |node| node.value == target }
+
+        nodes = [self]
+        until nodes.empty?
+            node = nodes.shift
+            return node if prc.call(node)
+            nodes.concat(nodes.children)
+        end
+        nil
+    end
+
+    def count
+        1 + children.map(&:count).inject(:+)
+    end
+end
+
+
 class PolyTreeNode
+    include Searchable
+
     attr_reader :parent
     attr_accessor :value
 
